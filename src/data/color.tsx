8d95bbff392812736a971e2846476fbd5987d023
fredriklexo <91253151+fredriklexo@users.noreply.github.com>
2022-05-24 10:56:01 +0200

    Fixed colors

---
+++ src/data/color.tsx
@@ -0,0 +1,14 @@
+export interface Colors {
+    primary: string,
+    secondary: string,
+    third: string,
+    fourth: string,
+}
+
+
+export const colors: Colors = {
+    primary: "#001244",
+    secondary: "#005086",
+    third: "#b0cac7",
+    fourth: "#f7d6bf"
+}
