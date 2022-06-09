02455b57a8e1f1455ec8561857a7601307700d7b
Angelica Moberg <angelica.moberg@hotmail.com>
2022-06-09 16:13:18 +0200

    missad context

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./common/footer"
 import ScrollToTop from "./common/scrollToTop"
 import ProductListProvider, { DeviceProvider, InvoiceInfoProvider } from "./context/provider"
 import CartProvider from "./context/cartProvider"
+import DeliveryProvider from "./context/deliveryProvider"
 
 
 const Layout: FC = () => {
@@ -14,10 +15,12 @@ const Layout: FC = () => {
           <DeviceProvider >
             <ProductListProvider>
               <CartProvider>
-                <ScrollToTop />
-                <Navbar />
-                <ContentContainer />
-                <Footer />
+                <DeliveryProvider>
+                  <ScrollToTop />
+                  <Navbar />
+                  <ContentContainer />
+                  <Footer />
+                </DeliveryProvider>
               </CartProvider>
             </ProductListProvider>
           </DeviceProvider>
