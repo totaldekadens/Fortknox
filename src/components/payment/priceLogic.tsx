f1576e50f46c2fa575da22231290c64ad43ec3de
Angelica Moberg <angelica.moberg@hotmail.com>
2022-06-14 19:47:08 +0200

    last fixes with taxes

--- src/components/payment/priceLogic.tsx
+++ src/components/payment/priceLogic.tsx
@@ -32,44 +32,44 @@ export function priceSummaryFunc(summary: string) {
 
      // tax a year
      if (summary === "moms.year") {
-        return (<> {totalPriceForIncludes * 0.25 * 12} </> )
+        return totalPriceForIncludes * 0.25 * 12
     }
     // tax a month
     if (summary === "moms.month") {
-        return (<> {totalPriceForIncludes * 0.25 } </> )
+        return totalPriceForIncludes * 0.25 
     }
     // tax delivery
     if (summary === "moms.delivery") {
-        return (<> {deliveryPrice * 0.25 } </> )
+        return deliveryPrice * 0.25 
     }
 
 
     //price exlusive tax a year
     if (summary === "ex.year") {
-        return (<> {totalPriceForIncludes * 12} </> )
+        return totalPriceForIncludes * 12
     }
     //price exlusive tax a month
     if (summary === "ex.month") {
-        return (<> {totalPriceForIncludes} </>)
+        return totalPriceForIncludes
     }
     //price inclusive tax a year
     if (summary === "inc.year") {
-        return (<> {totalPriceForIncludes * 12 * 1.25 } </> )
+        return totalPriceForIncludes * 12 * 1.25 
     }
     //price inclusive tax a month
     if (summary === "inc.month") {
-        return (<> {totalPriceForIncludes *  1.25} </> )
+        return totalPriceForIncludes *  1.25
     }
     //price exlusive tax a month + delivery price one time fee
     if (summary === "ex.totalAmount") {
-        return (<> {totalPriceForIncludes + deliveryPrice } </> )
+        return totalPriceForIncludes + deliveryPrice
     }
     //price inclusive tax a month + delivery price one time fee
     if (summary === "inc.totalAmount") {
         let month = totalPriceForIncludes *  1.25
         let deliveryCost = deliveryPrice * 1.25
         let sum = month + deliveryCost
-        return (<> {sum} </> )
+        return sum
     }
 
 }
